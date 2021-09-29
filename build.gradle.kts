4e4ca86c5d0a42fab8565ba11fc4f28f124fc417
begosrs <begtribot@gmail.com>
2021-09-29 17:01:53 +0100

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.72"
+    `java-gradle-plugin`
+    `maven-publish`
+}
+
+group = "org.tribot.begtribot"
+version = "1.0.2"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("net.lingala.zip4j:zip4j:2.6.4")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+gradlePlugin {
+    plugins {
+        create("TribotPlugin") {
+            id = "org.tribot.begtribot.tribot-gradle-plugin"
+            implementationClass = "org.tribot.begtribot.gradle.plugin.TribotPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/BegTribot/tribot-gradle-plugin")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("github.packages.user")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("github.packages.publishtoken")
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "tribot-gradle-plugin"
